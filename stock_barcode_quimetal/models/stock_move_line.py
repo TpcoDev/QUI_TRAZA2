5949b7c6680d00017887321f11a938f0f315ea78
Antonio Ruban <antoniodavid8@gmail.com>
2021-11-24 13:51:28 -0500

    [ADD] Etiquetas

--- stock_barcode_quimetal/models/stock_move_line.py
+++ stock_barcode_quimetal/models/stock_move_line.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import barcode
 from barcode.writer import ImageWriter
 
-from odoo import models, fields
+from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
@@ -29,62 +29,60 @@ class StockMoveLine(models.Model):
         string='Quimetal lines', required=False
     )
 
+    total_cant_envases = fields.Integer(compute='_compute_total_cant_envases', string='Total Cantidad de Envases')
+    total_peso_envase = fields.Float(compute='_compute_total_peso_envase', string='Total Peso/Envase')
+
+    @api.depends('quimetal_lines_ids.cant_envases')
+    def _compute_total_cant_envases(self):
+        for rec in self:
+            rec.total_cant_envases = sum(rec.quimetal_lines_ids.mapped('cant_envases'))
+
+    @api.depends('quimetal_lines_ids.peso_envase', 'quimetal_lines_ids.cant_envases', 'quimetal_lines_ids.num_bultos')
+    def _compute_total_peso_envase(self):
+        sum_list = []
+        for rec in self:
+            for line in rec.quimetal_lines_ids:
+                sum_list.append(line.peso_envase * line.cant_envases * line.num_bultos)
+            rec.total_peso_envase = sum(sum_list)
+
     def export_pdf(self):
         context = self._context
         datas = {'ids': context.get('active_ids', [])}
-        tipos = self._context.get('tipo')
+        tipos = 1 if self.product_id.as_type_product == 'MP' else 0
+        as_cantidades = 67
+
+        calculo = datos_producto.as_contenido_envase * datos_producto.as_cantidad_envase * datos_producto.as_cantidad_unidades
+        kg_compra = move.qty_done
+        if calculo > 0:
+            total = kg_compra / calculo
+        else:
+            total = kg_compra
+        total = math.ceil(total)
+
+
         datas['model'] = 'as.wizard.formulas'
         datas['form'] = self.read()[0]
         diccionario = []
         # tipo 1 move
-        if self._context.get('tipo') == 1:
-            for contexto_lotes in self.wiz_lineas:
-                if self.modelo != "stock.production.lot":
-                    file_like_object = BytesIO()
-                    EAN = barcode.get_barcode_class('code128')
-                    ean = EAN(contexto_lotes.operacion_id.as_barcode_mpp_1_CDB(), writer=ImageWriter())
-                    ean.write(file_like_object, options={"write_text": False})
-                    contexto_lotes.operacion_id.as_imge = base64.b64encode(file_like_object.getvalue())
-                    for i in range(0, contexto_lotes.as_cantidades):
-                        diccionario.append(contexto_lotes.operacion_id.id)
-                else:
-                    move_id = self.env['stock.move.line'].search([('lot_id', '=', contexto_lotes.lots_id.id)],
-                                                                 order='date asc', limit=1)
-                    if not move_id:
-                        raise UserError("El producto no posee Stock")
-                    file_like_object = BytesIO()
-                    EAN = barcode.get_barcode_class('code128')
-                    ean = EAN(move_id.as_barcode_mpp_1_CDB(), writer=ImageWriter())
-                    ean.write(file_like_object, options={"write_text": False})
-                    move_id.as_imge = base64.b64encode(file_like_object.getvalue())
-                    for i in range(0, contexto_lotes.as_cantidades):
-                        diccionario.append(move_id.id)
+        if tipos == 1:
+            file_like_object = BytesIO()
+            EAN = barcode.get_barcode_class('code128')
+            ean = EAN(self.as_barcode_mpp_1_CDB(), writer=ImageWriter())
+            ean.write(file_like_object, options={"write_text": False})
+            self.as_imge = base64.b64encode(file_like_object.getvalue())
+            for i in range(0, as_cantidades):
+                diccionario.append(self.id)
             if diccionario == []:
                 raise UserError("Debe guardar el formulario para generar reporte")
             return self.env.ref('as_stock_equimetal.as_reporte_2').report_action(diccionario)
         else:
-            for contexto_lotes in self.wiz_lineas:
-                if self.modelo != "stock.production.lot":
-                    file_like_object = BytesIO()
-                    EAN = barcode.get_barcode_class('code128')
-                    ean = EAN(contexto_lotes.operacion_id.as_barcode_pp_1_CDB(), writer=ImageWriter())
-                    ean.write(file_like_object, options={"write_text": False})
-                    contexto_lotes.operacion_id.as_imge = base64.b64encode(file_like_object.getvalue())
-
-                    for i in range(0, contexto_lotes.as_cantidades):
-                        diccionario.append(contexto_lotes.operacion_id.id)
-                else:
-                    move_id = self.env['stock.move.line'].search([('lot_id', '=', contexto_lotes.lots_id.id)],
-                                                                 order='date asc', limit=1)
-                    if not move_id:
-                        raise UserError("El producto no posee Stock")
-                    file_like_object = BytesIO()
-                    EAN = barcode.get_barcode_class('code128')
-                    ean = EAN(move_id.as_barcode_pp_1_CDB(), writer=ImageWriter())
-                    ean.write(file_like_object, options={"write_text": False})
-                    move_id.as_imge = base64.b64encode(file_like_object.getvalue())
-                    for i in range(0, contexto_lotes.as_cantidades):
-                        diccionario.append(move_id.id)
+            file_like_object = BytesIO()
+            EAN = barcode.get_barcode_class('code128')
+            ean = EAN(self.as_barcode_pp_1_CDB(), writer=ImageWriter())
+            ean.write(file_like_object, options={"write_text": False})
+            self.as_imge = base64.b64encode(file_like_object.getvalue())
+            for i in range(0, as_cantidades):
+                diccionario.append(self.id)
 
             if diccionario == []:
                 raise UserError("Debe guardar el formulario para generar reporte")
